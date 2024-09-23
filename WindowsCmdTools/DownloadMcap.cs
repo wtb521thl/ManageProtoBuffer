aa31c1446cc5b85ee4df96105c2a9e01ff52826f
wangtianbo <wangtianbo@megvii.com>
2024-09-23 16:15:19 +0800

    改名WindowsCmdTools，添加下载s3数据功能

---
+++ WindowsCmdTools/DownloadMcap.cs
@@ -0,0 +1,42 @@
+﻿
+namespace MultiplyManageProtoBuffer
+{
+    internal class DownloadMcap
+    {
+        public void DownloadS3Mcap(string? s3DownloadPath, string? targetPath)
+        {
+            if (string.IsNullOrEmpty(s3DownloadPath))
+            {
+                Console.WriteLine("输入地址不仅能为空");
+                return;
+            }
+            if (string.IsNullOrEmpty(targetPath))
+            {
+                targetPath = "C:\\Users\\wangtianbo\\Desktop\\e2e测试数据\\Mcap数据\\" + Path.GetFileName(s3DownloadPath);
+                Console.WriteLine("目标路径为空，自动设置为：" + targetPath);
+            }
+            Command command = new Command();
+            command.Output += Command_Output;
+            command.Error += Command_Error;
+            command.Exited += Command_Exited;
+            command.RunCMD($"aws --endpoint-url=http://oss-internal.i.brainpp.cn:80 s3 cp {s3DownloadPath} {targetPath}");
+
+            Console.ReadLine();
+        }
+
+        void Command_Exited()
+        {
+            Console.WriteLine("已退出");
+        }
+
+        void Command_Error(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        void Command_Output(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ WindowsCmdTools/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MultiplyManageProtoBuffer;
+
+Console.WriteLine("选择功能：1（protobuff），2（下载mcap）");
+var keyInfo = Console.ReadKey();
+switch (keyInfo.Key)
+{
+    case ConsoleKey.D1
+        :
+        ProToCsharp proToCsharp = new ProToCsharp();
+        proToCsharp.StartCommand();
+        break;
+    case ConsoleKey.D2
+        :
+        Console.WriteLine("当前为下载Mcap模块，请输入下载地址");
+        string? tempDownloadPath = Console.ReadLine();
+        Console.WriteLine("请输入本地储存地址");
+        string? tempTargetPath = Console.ReadLine();
+        DownloadMcap downloadMcap = new DownloadMcap();
+        downloadMcap.DownloadS3Mcap(tempDownloadPath, tempTargetPath);
+        break;
+}
+
+
+
+
+
+
+
+
